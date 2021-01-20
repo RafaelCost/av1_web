913c95fb90c24b9b2126074678e22e5c4d8ffde0
RafaelCosta <49794031+RafaelCost@users.noreply.github.com>
2021-01-20 18:48:13 -0300

    ultimas alterações

--- js/changeBackgroundColor.js
+++ js/changeBackgroundColor.js
@@ -2,14 +2,17 @@
 
 function set_Color(num) {
     if(num == 1) {
-        document.querySelector("body").style.background = "Black";
+        document.querySelector(".all").style.background = "Black";
+        // document.querySelector("body").style.background = "linear-gradient(to right, rgb(0, 0, 0), rgb(53, 53, 53))";
     }
 
     if(num == 2) {
-        document.querySelector("body").style.background = "Red";
+        document.querySelector(".all").style.background = "Red";
+        // document.querySelector("body").style.background = "linear-gradient(to right, rgb(174, 0, 0), rgb(228, 65, 65))";
     }
 
     if(num == 3) {
-        document.querySelector("body").style.background = "Purple";
+        document.querySelector(".all").style.background = "Purple";
+        // document.querySelector("body").style.background = "linear-gradient(to right, rgb(72, 0, 79), rgb(107, 41, 130))";
     }
 }
